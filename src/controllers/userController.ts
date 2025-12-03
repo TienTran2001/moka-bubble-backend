9981d25820b2a08c015860eadf0e1b6230224357
tientv <tienco201@gmail.com>
2025-12-03 10:29:27 +0700

    Refactor userController to utilize new IRequest interface for user data and move interface definition to a separate request types file.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,6 @@
-import { Request, Response } from 'express'
-import { IUser } from '~/models'
+import { Response } from 'express'
+import { IRequest } from '~/types/request'
 import { sendResponse } from '~/utils/responseHelper'
-
-interface IRequest extends Request {
-  user?: IUser
-}
 
 const authMe = (req: IRequest, res: Response) => {
   const user = req.user
