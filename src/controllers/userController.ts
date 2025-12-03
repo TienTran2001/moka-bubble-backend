ca11044a5a8443e0ab88c882d8e471c1933a3a1a
tientv <tienco201@gmail.com>
2025-12-03 09:43:02 +0700

    Implement (user/me) get profile

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from 'express'
+import { IUser } from '~/models'
 import { sendResponse } from '~/utils/responseHelper'
 
-const authMe = (req: Request, res: Response) => {
-  return sendResponse.success(res, 'user')
+interface IRequest extends Request {
+  user?: IUser
+}
+
+const authMe = (req: IRequest, res: Response) => {
+  const user = req.user
+  if (!user) return sendResponse.error(res, 'User not found', 404)
+
+  return sendResponse.success(res, { me: user }, 'Get user information successfully')
 }
 
 export default {
